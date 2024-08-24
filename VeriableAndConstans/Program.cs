680cc66ce1d4f29f18f602b88796537d191fbc9e
azizallaev78@gmail.com <azizallaev78@gmail.com>
2024-08-24 23:56:36 +0500

    Added two new lessons

---
+++ VeriableAndConstans/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace VeriableAndConstans
+{
+	internal class Program
+	{
+		//Объявили константу вне метода Main это важно
+		public const double Pi = 3.14;
+
+
+		//Выводим константу Pi
+		static void Main(string[] args)
+		{ 
+			Console.WriteLine($"Константа Pi равна {Pi}");
+		}
+
+		
+
+
+			
+	}
+}
+
+
+/*Объявлять константу обязательно за всеми метода и классами 
+  Потому что это общее значение которое не меняется в коде
+ */
