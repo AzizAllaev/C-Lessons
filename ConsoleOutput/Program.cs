c870cae9ff09277dde8e849792830898693f325e
azizallaev78@gmail.com <azizallaev78@gmail.com>
2024-08-26 16:41:31 +0500

    New theme filled

--- ConsoleOutput/Program.cs
+++ ConsoleOutput/Program.cs
@@ -4,7 +4,44 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Привет Вася!");
+			Console.Write("Сколько лет Васе: ");
+
+
+					//Помимо привычного метода WriteLine также есть Write
+			
+			string VasyaAge = Console.ReadLine();  
+			Console.WriteLine($"Васе {VasyaAge} лет");
+
+					//Отличается он тем что при выполнении он не переходит на новую строку
+
+			int VasyaAgeInt = int.Parse(VasyaAge);
+
+
+					//Также тут я хотел закрепить метод Convert.ToInt32
+
+			Console.WriteLine("Сколько лет Пете?");
+			int Petyagrade = Convert.ToInt32(Console.ReadLine());
+		
+
+			int age = Petyagrade - VasyaAgeInt;
+
+
+			if (age > 0)
+			{
+				Console.WriteLine($"Петя старше на {age} лет");
+			}
+			else
+			{
+				int Secondage = age * -1;
+				Console.WriteLine($"Вася старше на {Secondage} лет");
+			}
+		
+
+			//Метод Convert.ToInt32 хорошая альтернатива int.Parse()
+
+
+			
 		}
 	}
 }
