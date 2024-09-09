4f490bc5f84deeb7baaf7cc2c959973d43f3fadf
azizallaev78@gmail.com <azizallaev78@gmail.com>
2024-09-09 16:46:17 +0500

    New theme Arrays Also "Cycles" has been changed

---
+++ Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Arrays
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] human = { 1, 2, 3 };
+			Console.Write("Какой у вас вес: ");
+			string ConvertConsole = Console.ReadLine();
+			int weight = Convert.ToInt32(ConvertConsole);
+			human[0] = weight;
+
+			Console.Write("Какой у вас рост: ");
+			string ConvertHeight = Console.ReadLine();
+			int height = Convert.ToInt32(ConvertHeight);
+			human[1] = height;
+
+			Console.Write("Какой у вас возраст: ");
+			string ConvertAge = Console.ReadLine();
+			int age = Convert.ToInt32(ConvertAge);
+			human[2] = age;
+
+			Console.WriteLine($"Возраст: {human[0]}");
+			Console.WriteLine($"Вес: {human[1]}");
+			Console.WriteLine($"Рост: {human[2]}");
+
+			
+		}
+	}
+}
--- Cycles/Program.cs
+++ Cycles/Program.cs
@@ -4,35 +4,58 @@
 	{
 		static void Main(string[] args)
 		{
-			for (int i = 10; i > 0; i--)
+			bool IsRunning = true;
+			bool IsRunning2 = true;
+
+
+			Console.WriteLine("Чтобы запустить програграмму напишите <Enter>...");
+			string Enter = Console.ReadLine();
+
+			if (Enter == "<Enter>")
 			{
-				Console.WriteLine(i);
+				do
+				{
+					int[] human = { 1, 2, 3 };
+					Console.Write("Какой у вас вес: ");
+					string ConvertConsole = Console.ReadLine();
+					int weight = Convert.ToInt32(ConvertConsole);
+					human[0] = weight;
+
+					Console.Write("Какой у вас рост: ");
+					string ConvertHeight = Console.ReadLine();
+					int height = Convert.ToInt32(ConvertHeight);
+					human[1] = height;
+
+					Console.Write("Какой у вас возраст: ");
+					string ConvertAge = Console.ReadLine();
+					int age = Convert.ToInt32(ConvertAge);
+					human[2] = age;
+
+					Console.WriteLine($"Возраст: {human[0]}");
+					Console.WriteLine($"Вес: {human[1]}");
+					Console.WriteLine($"Рост: {human[2]}");
+				}
+				while (IsRunning2 = false);
+
+
+
+			}
+			else
+			{
+				Console.WriteLine("Чтобы запустить вам надо написать <Enter>, если хотите завершить работу программы то напишите Stop");
+				string check = Console.ReadLine();
+				if (check == "Stop")
+				{
+					IsRunning = true;
+				}
 			}
 
 
 
-			int ab = 10;
-			do
-			{
-				Console.WriteLine(ab);
-				ab--;
-			}
-			while (ab > 0);
 
 
 
-			int ac = 20;
-			while (ac > 0)
-			{
-				Console.WriteLine(ac);
-				ac--;
-			}
 
-			
-
-
-
-			Console.ReadLine();
 		}
 	}
 }
