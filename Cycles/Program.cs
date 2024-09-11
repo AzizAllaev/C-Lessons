a6c498a64d9b48b3df3ee2d8a6156bd2b86f7e75
azizallaev78@gmail.com <azizallaev78@gmail.com>
2024-09-11 19:55:33 +0500

    New theme Methods

--- Cycles/Program.cs
+++ Cycles/Program.cs
@@ -13,31 +13,25 @@
 
 			if (Enter == "<Enter>")
 			{
-				do
-				{
-					int[] human = { 1, 2, 3 };
-					Console.Write("Какой у вас вес: ");
-					string ConvertConsole = Console.ReadLine();
-					int weight = Convert.ToInt32(ConvertConsole);
-					human[0] = weight;
+				int[] human = { 1, 2, 3 };
+				Console.Write("Какой у вас вес: ");
+				string ConvertConsole = Console.ReadLine();
+				int weight = Convert.ToInt32(ConvertConsole);
+				human[0] = weight;
 
-					Console.Write("Какой у вас рост: ");
-					string ConvertHeight = Console.ReadLine();
-					int height = Convert.ToInt32(ConvertHeight);
-					human[1] = height;
+				Console.Write("Какой у вас рост: ");
+				string ConvertHeight = Console.ReadLine();
+				int height = Convert.ToInt32(ConvertHeight);
+				human[1] = height;
 
-					Console.Write("Какой у вас возраст: ");
-					string ConvertAge = Console.ReadLine();
-					int age = Convert.ToInt32(ConvertAge);
-					human[2] = age;
+				Console.Write("Какой у вас возраст: ");
+				string ConvertAge = Console.ReadLine();
+				int age = Convert.ToInt32(ConvertAge);
+				human[2] = age;
 
-					Console.WriteLine($"Возраст: {human[0]}");
-					Console.WriteLine($"Вес: {human[1]}");
-					Console.WriteLine($"Рост: {human[2]}");
-				}
-				while (IsRunning2 = false);
-
-
+				Console.WriteLine($"Возраст: {human[0]}");
+				Console.WriteLine($"Вес: {human[1]}");
+				Console.WriteLine($"Рост: {human[2]}");
 
 			}
 			else
---
+++ Methods/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace Methods
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Если вы хотите сложить напишиете: +");
+				Console.WriteLine("Если вы хотите отнять напишите: -");
+				Console.WriteLine("Если вы хотите умножить напишите: *");
+				Console.WriteLine("Если вы хотите разделить напишите: /");
+				Console.Write("Какое действие вас интересует: ");
+				string check = Console.ReadLine();
+
+				if (check == "+")
+				{
+					Console.Write("Напишите первое число: ");
+					string convpl1 = Console.ReadLine();
+					int firstPl = Convert.ToInt32(convpl1);
+
+					Console.Write("Напишите второе число: ");
+					string convpl2 = Console.ReadLine();
+					int secondPl = Convert.ToInt32(convpl2);
+
+					int resultPl = Plus(secondPl, firstPl);
+
+					Console.WriteLine($"Результат: {firstPl} + {secondPl} = {resultPl}");
+				}
+				else if (check == "-")
+				{
+					Console.Write("Напишите первое число: ");
+					string convmin1 = Console.ReadLine();
+					int firstMin = Convert.ToInt32(convmin1);
+
+					Console.Write("Напишите второе число:");
+					string convmin2 = Console.ReadLine();
+					int secondMin = Convert.ToInt32(convmin2);
+
+					int resultMin = Minus(firstMin, secondMin);
+
+					Console.WriteLine($"Результат: {firstMin} - {secondMin} = {resultMin}");
+				}
+				else if (check == "*")
+				{
+					Console.Write("Напишите первое число: ");
+					string convmul = Console.ReadLine();
+					int firstMul = Convert.ToInt32(convmul);
+
+					Console.Write("Напишите второе число: ");
+					string convmul2 = Console.ReadLine();
+					int secondMul = Convert.ToInt32(convmul2);
+
+					int resultMul = Multiply(firstMul, secondMul);
+
+					Console.WriteLine($"Результат: {firstMul} * {secondMul} = {resultMul}");
+				}
+				else if (check == "/")
+				{
+					Console.Write("Напишите первое число: ");
+					string convsp1 = Console.ReadLine();
+					int firstSp = Convert.ToInt32(convsp1);
+
+					Console.Write("Напишите второе число: ");
+					string convsp2 = Console.ReadLine();
+					int secondSp = Convert.ToInt32(convsp2);
+
+
+					int resultSp = Split(firstSp, secondSp);
+
+					Console.WriteLine($"Результат: {firstSp} / {secondSp} = {resultSp}");
+				}
+				else 
+				{
+					Console.WriteLine("Следуйте инструкции сверху, если хотите закончить работу программы напишите Stop");
+					string stop = Console.ReadLine();
+					if (stop == "Stop")
+					{
+						break;
+					}
+				}
+
+
+
+
+
+
+
+				//Методы
+
+				static int Plus(int a, int b)
+				{
+					return a + b;
+				}
+				static int Minus(int ab, int bc)
+				{
+					return ab - bc;
+				}
+				static int Multiply(int abc, int abd)
+				{
+					return abc * abd;
+				}
+				static int Split(int bca, int bda)
+				{
+					return bca / bda;
+				}
+			}
+			
+
+
+
+
+
+
+		}
+	}
+}
